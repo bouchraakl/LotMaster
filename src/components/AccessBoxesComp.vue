44544436a0b81de86edd23a7b6e79554e2109031
Bouchra Akl <bushraakl1234@gmail.com>
2023-06-11 12:51:16 -0300

    components | reusable UI implemented.

---
+++ src/components/AccessBoxesComp.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="d-flex flex-wrap justify-content-center custom-style">
+    <div v-for="box in boxes" :key="box.id" class="box" @click="navigateToPage(box.route)">
+      <div class="box-content d-flex flex-column justify-content-start align-items-start">
+        <div class="box-text-top">{{ getBoxTextTop(box.name) }}</div>
+        <div class="box-text-bottom">{{ getBoxTextBottom(box.name) }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.custom-style {
+  margin-top: 30px;
+}
+
+.box {
+  width: 257px;
+  height: 237px;
+  margin: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-start;
+  cursor: pointer;
+  background: #3C3C43;
+  border-radius: 24px;
+  transition: transform 0.3s ease, box-shadow 0.3s ease;
+}
+
+.box:hover {
+  transform: translateY(-5px);
+  box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.1);
+}
+
+.box-content {
+  padding: 10px;
+  padding-left: 20px;
+  font-family: 'Raleway';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  color: #D9D9D9;
+}
+
+.box-text-top {
+  text-align: center;
+}
+
+.box-text-bottom {
+  text-align: center;
+}
+
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+}
+
+.box-enter {
+  animation: fadeIn 0.5s ease;
+  opacity: 0;
+}
+
+.box-leave-to {
+  opacity: 0;
+}
+</style>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { RouterLinkProps } from 'vue-router';
+
+export default defineComponent({
+  data() {
+    return {
+      boxes: [
+        { id: 1, name: 'Open Movements', route: '/access-movement' },
+        { id: 2, name: 'Closed Movements', route: '/access-closemovement' },
+        { id: 3, name: 'Vehicle Brands', route: '/access-vehicleBrand' },
+        { id: 4, name: 'Vehicle Models', route: '/access-vehicleModel' },
+        { id: 5, name: 'Vehicles', route: '/access-vehicle' },
+        { id: 6, name: 'Conductors', route: '/access-conductor' },
+      ],
+    };
+  },
+  methods: {
+    navigateToPage(route: string): void {
+      this.$router.push(route);
+      console.log('Navigating to', route);
+    },
+    getBoxTextTop(name: string): string {
+      const parts = name.split(' ');
+      return parts[0]; // Retrieves the first word from the name
+    },
+    getBoxTextBottom(name: string): string {
+      const parts = name.split(' ');
+      return parts.slice(1).join(' '); // Retrieves the remaining words and joins them with a space
+    },
+  },
+});
+</script>
---
+++ src/components/SideBarComp.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="d-flex flex-column align-items-center p-4 bg-light h-100 sidebar" style="width: 200px;">
+    <div class="d-flex align-items-center justify-content-center mb-md-0 me-md-auto link-dark text-decoration-none"
+      style="width: 100%;">
+      <span class="logo">LOTMASTER</span>
+    </div>
+    <hr class="w-100">
+    <ul class="nav nav-pills flex-column justify-content-start align-items-start gap-4 pt-4 no-bullets">
+      <li class="nav-item d-flex justify-content-center align-items-center custom-li" :class="{ 'active': activeLink === 'dashboard' }"
+        @click="activeLink = 'dashboard'">
+        <i class="bi bi-grid-1x2" width="16" height="16"></i>
+        <router-link to="/" class="custom-link text-decoration-none">Dashboard</router-link>
+      </li>
+      <li class="nav-item d-flex justify-content-center align-items-center custom-li" :class="{ 'active': activeLink === 'register' }"
+        @click="activeLink = 'register'">
+        <i class="bi bi-clipboard-plus" width="16" height="16"></i>
+        <router-link to="/register" class="custom-link text-decoration-none">Register</router-link>
+      </li>
+      <li class="nav-item d-flex justify-content-center align-items-center custom-li" :class="{ 'active': activeLink === 'access' }" @click="activeLink = 'access'">
+        <i class="bi bi-box-arrow-up-right" width="16" height="16"></i>
+        <router-link to="/access" class="custom-link text-decoration-none">Access</router-link>
+      </li>
+      <li class="nav-item d-flex justify-content-center align-items-center custom-li" :class="{ 'active': activeLink === 'controle' }"
+        @click="activeLink = 'controle'">
+        <i class="bi bi-toggles" width="16" height="16"></i>
+        <router-link to="/controle" class="custom-link text-decoration-none">Controle</router-link>
+      </li>
+    </ul>
+    <hr>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'SideBarComp',
+  data() {
+    return {
+      activeLink: 'dashboard' // Set the initial active link here
+    };
+  },
+  created() {
+    this.setActiveLink();
+  },
+  methods: {
+    setActiveLink() {
+      // Get the current URL
+      const currentURL = window.location.href;
+
+      // Check if the URL contains a specific string and set the active link accordingly
+      if (currentURL.includes('register')) {
+        this.activeLink = 'register';
+      } else if (currentURL.includes('access')) {
+        this.activeLink = 'access';
+      } else if (currentURL.includes('controle')) {
+        this.activeLink = 'controle';
+      } else {
+        this.activeLink = 'dashboard';
+      }
+    }
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.sidebar {
+  height: 100%;
+}
+
+.no-bullets {
+  list-style: none;
+}
+
+.logo {
+  text-align: center;
+  font-family: 'Nunito', sans-serif;
+  font-size: 1.3rem;
+}
+
+.bi {
+  margin-right: 10px;
+}
+
+.custom-link {
+  font-family: 'Raleway', sans-serif;
+  font-size: 15px;
+}
+
+a {
+  color: #000;
+}
+
+.nav-pills .custom-li{
+  width: 160px;
+height: 40px;
+}
+
+.nav-pills .custom-li.active {
+  background-color: black;
+  border-radius: 24px;
+  /* Set the background color of custom-li when custom-link is active */
+}
+
+.nav-pills .custom-li.active i {
+  color: #fff;
+}
+
+.nav-pills .custom-li.active .custom-link {
+  color: #fff;
+}
+</style>
+
