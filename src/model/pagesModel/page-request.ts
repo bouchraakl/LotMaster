54427ca78792f0965d50aa63647081b909f1a13d
Bouchra Akl <bushraakl1234@gmail.com>
2023-06-13 11:49:10 -0300

    model | page request finilized.

--- src/model/pagesModel/page-request.ts
+++ src/model/pagesModel/page-request.ts
@@ -1,19 +1,12 @@
 export class PageRequest {
-    filter!: string
     currentPage!: number
     pageSize!: number
-
-    sortField!: string
     direction!: string
 
     constructor() {
-      this.filter = ''
       this.currentPage = 0
-      this.pageSize = 10
+      this.pageSize = 2
       this.direction = 'asc'
     }
 
-    public isSortable(): boolean {
-      return this.sortField !== undefined && this.direction !== undefined
-    }
 }
