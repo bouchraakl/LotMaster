45feac7e9eaa7a4c8e11a4025b512ca6799376ab
Bouchra Akl <bushraakl1234@gmail.com>
2023-06-14 09:43:29 -0300

    view | register form frontend vehicle model done.

--- src/views/car-models/RegisterVehicleModelView.vue
+++ src/views/car-models/RegisterVehicleModelView.vue
@@ -1,1 +1,63 @@
-<template></template>
+<template>
+    <div class="access-content d-flex flex-column align-items-start justify-content-start">
+      <p class="title-pages">Register : Vehicle Model</p>
+      <div class="form-application d-flex flex-column custom-section">
+        <form class="form-app d-flex flex-column align-items-start mt-4 h-100 gap-3" @submit.prevent="">
+          <div class="d-flex align-items-center align-self-start gap-3">
+            <div class=" d-flex flex-column">
+              <label for="brands" class="form-label">Associated Brand</label>
+              <input class="form-control" list="datalistOptions" id="brands" style="width: 300px;">
+              <datalist id="datalistOptions">
+              <option value="San Francisco"></option>
+            </datalist>
+            </div>
+          </div>
+          <div class="d-flex align-items-center align-self-start gap-3">
+            <div class=" d-flex flex-column">
+              <label for="modelname" class="form-label">Model Name </label>
+              <input class="form-control" id="modelname" style="width: 300px;">
+            </div>
+          </div>
+          <button class="mt-3">Register Vehicle Model</button>
+        </form>
+      </div>
+    </div>
+  </template>
+    
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  
+  export default defineComponent({
+    data() {
+      return {
+        datalistId: "suggestions",
+      };
+    },
+  })
+  
+  </script>
+  
+  <style>
+  .moveBtn {
+    width: 258px;
+    height: 37px;
+    border-radius: 5px;
+    border: none;
+    font-family: 'Raleway';
+    font-weight: 500;
+    font-size: 15px;
+    background: #B5C2C9;
+    color: #F3F3F3;
+    margin: 0;
+  }
+  
+  label {
+    font-family: Raleway;
+    font-size: 15px;
+    font-weight: 400;
+    line-height: 20px;
+    text-align: left;
+  
+  }
+  </style>
+    
