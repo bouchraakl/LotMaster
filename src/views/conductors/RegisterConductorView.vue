86a76969535682836ae444fa8a744a13060db6f0
Bouchra Akl <bushraakl1234@gmail.com>
2023-06-15 18:56:45 -0300

    conductor | post form done.

--- src/views/conductors/RegisterConductorView.vue
+++ src/views/conductors/RegisterConductorView.vue
@@ -2,35 +2,55 @@
     <div class="access-content d-flex flex-column align-items-start justify-content-start">
       <p class="title-pages">Register : Conductor</p>
       <div class="form-application d-flex flex-column custom-section">
-        <form class="form-app d-flex flex-column align-items-start mt-4 h-100 gap-3" @submit.prevent="">
+        <form class="form-app d-flex flex-column align-items-start mt-4 h-100 gap-3" @submit.prevent="submitForm">
           <div class="d-flex align-items-center align-self-start gap-3">
             <div class=" d-flex flex-column">
               <label for="name" class="form-label">Conductor Name</label>
-              <input class="form-control" id="name" style="width: 300px;">
+              <input class="form-control" id="name" style="width: 300px;" v-model="condutor.nome">
             </div>
             <div class=" d-flex flex-column">
               <label for="cpf" class="form-label">Conductor CPF</label>
-              <input class="form-control" id="cpf" style="width: 300px;"></div>
+              <input class="form-control" id="cpf" style="width: 300px;" v-model="condutor.cpf"></div>
           </div>
           <div class="d-flex align-items-center align-self-start gap-3">
             <div class=" d-flex flex-column">
               <label for="phone" class="form-label">Conductor Phone</label>
-              <input class="form-control" id="phone" style="width: 300px;"></div>
+              <input class="form-control" id="phone" style="width: 300px;" v-model="condutor.telefone"></div>
           </div>
-          <button class="mt-4">Register Conductor</button>
+          <button class="mt-4" type="submit">Register Conductor</button>
         </form>
       </div>
     </div>
   </template>
     
   <script lang="ts">
-  import { defineComponent } from 'vue';
+  import { CondutorClient } from '@/client/condutor.client';
+import { Condutor } from '@/model/condutor';
+import { defineComponent } from 'vue';
   
   export default defineComponent({
     data() {
       return {
-        datalistId: "suggestions",
+        condutor: new Condutor(),
+
       };
+    },
+    computed: {
+      condutorClient() {
+        return new CondutorClient();
+      },
+    },
+    methods: {
+      async submitForm() {
+        try {
+        const response = await this.condutorClient.save(this.condutor);
+        const data = response;
+        console.log(data);
+      } catch (error) {
+        console.log("Erro ao salvar condutor", this.condutor);
+        console.error(error);
+      }
+      },
     },
   })
   
