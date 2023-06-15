f9177e1bbc19563b59b87442fee9e31665d677bd
Bouchra Akl <bushraakl1234@gmail.com>
2023-06-15 10:06:15 -0300

    vehicle brands | post from done.

--- src/views/car-brands/RegisterVehicleBrandView.vue
+++ src/views/car-brands/RegisterVehicleBrandView.vue
@@ -1,31 +1,49 @@
 <template>
-    <div class="access-content d-flex flex-column align-items-start justify-content-start">
-      <p class="title-pages">Register : Vehicle Brand</p>
-      <div class="form-application d-flex flex-column custom-section">
-        <form class="form-app d-flex flex-column align-items-start mt-4 h-100 gap-3" @submit.prevent="">
-          <div class="d-flex align-items-center align-self-start gap-3">
-            <div class=" d-flex flex-column">
-              <label for="brand" class="form-label">Vehicle Brand Name</label>
-              <input class="form-control" id="brand" style="width: 300px;">
-            </div>
+  <div class="access-content d-flex flex-column align-items-start justify-content-start">
+    <p class="title-pages">Register : Vehicle Brand</p>
+    <div class="form-application d-flex flex-column custom-section">
+      <form class="form-app d-flex flex-column align-items-start mt-4 h-100 gap-3" @submit.prevent="submitForm">
+        <div class="d-flex align-items-center align-self-start gap-3">
+          <div class=" d-flex flex-column">
+            <label for="brand" class="form-label">Vehicle Brand Name</label>
+            <input class="form-control" id="brand" style="width: 300px;" v-model="brand.nome">
           </div>
-          <button class="mt-3">Register Brand Name</button>
-        </form>
-      </div>
+        </div>
+        <button class="mt-3" type="submit">Register Brand Name</button>
+      </form>
     </div>
-  </template>
+  </div>
+</template>
     
-  <script lang="ts">
-  import { defineComponent } from 'vue';
-  
-  export default defineComponent({
-    data() {
-      return {
-      };
-    },
-  })
-  
-  </script>
+<script lang="ts">
+    import { MarcaClient } from '@/client/marca.client';
+    import { Marca } from '@/model/marca';
+    import { defineComponent } from 'vue';
+    
+    export default defineComponent({
+      data() {
+        return {
+          brand: new Marca(),
+        };
+      },
+      computed: {
+        brandClient() {
+          return new MarcaClient();
+        },
+      },
+      methods: {
+        async submitForm() {
+          try {
+            const response = await this.brandClient.save(this.brand);
+            const data = response;
+            console.log(data);
+          } catch (error) {
+            console.log('Erro ao salvar marca', this.brand);
+          }
+        },
+      },
+    })
+    </script>
   
   <style>
   .moveBtn {
