489b82e52399bafe278675ec3f305a17336310b8
Bouchra Akl <bushraakl1234@gmail.com>
2023-06-26 10:58:06 -0300

    deploying application.

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env sh
 
+# abort on errors
 set -e
 
+# build
 npm run build
 
+# navigate into the build output directory
 cd dist
 
-git add -A
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
 
+git init
+git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:bouchraakl/LotMaster.git master:gh-pages
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/bouchraakl/LotMaster.git main:gh-pages
 
-cd - 
-
+cd -
